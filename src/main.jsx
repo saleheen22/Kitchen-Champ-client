3b33f0ec7d0999b522e4555d0b98acf0810c1f29
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-06-10 12:36:00 +0600

    useAuth hook added for Authcontext of AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,22 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './providers/AuthProvider'
 const queryClient = new QueryClient()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </HelmetProvider><HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
