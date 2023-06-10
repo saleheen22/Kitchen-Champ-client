6c68ef7b95051ebd7ecd2024be8ff99ab6e8e6b8
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-06-10 21:46:50 +0600

    Registration page done

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,77 +1,165 @@
 import { Helmet } from "react-helmet";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SocialLogin from "../Login/SocialLogin";
+import useAuth from "../../hooks/useAuth";
+import { useRef } from "react";
+import Swal from "sweetalert2";
 
 const Registration = () => {
-    const { register, reset, handleSubmit, formState: { errors } } = useForm();
+    const { register, reset, handleSubmit, watch, formState: { errors } } = useForm();
+    const { createUser, updateUserProfile, loading } = useAuth();
+    const navigate = useNavigate();
+    const onSubmit = data => {
+        console.log(data);
+
+        createUser(data.email, data.password)
+            .then(result => {
+
+                const loggedUser = result.user;
+                console.log(loggedUser);
+
+                updateUserProfile(data.name, data.photoURL)
+                    .then(() => {
+                        const saveUser = { name: data.name, email: data.email }
+                        fetch('http://localhost:5000/users', {
+                            method: 'POST',
+                            headers: {
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify(saveUser)
+                        })
+                            .then(res => res.json())
+                            .then(data => {
+                                if (data.insertedId) {
+                                    reset();
+                                    Swal.fire({
+                                        position: 'top-end',
+                                        icon: 'success',
+                                        title: 'User created successfully.',
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    });
+                                    navigate('/');
+                                }
+                            })
+
+
+
+                    })
+                    .catch(error => console.log(error))
+            })
+    }
+    const password = useRef({});
+    password.current = watch("password", "");
+
     return (
         <div className="mt-28">
-        <Helmet>
-            <title>Kitchen Champ || Register</title>
-        </Helmet>
-        <div>
-        <div>
-            <h2 className='text-center text-3xl'>Register Champ!! </h2>
-            <form >
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text"  name="name" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text"  name="name" className="input input-bordered" />
-                    </div>    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text"  name="name" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text"  name="name" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text"  name="name" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text"  name="name" className="input input-bordered" />
-                    </div>
 
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="text" name="email" placeholder="email" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Due amount</span>
-                        </label>
-                        <input type="text" className="input input-bordered" />
-                    </div>
-                </div>
-                <div className="form-control mt-6 mb-10 w-3/5  mx-auto">
-                    <input className="btn btn-warning btn-block" type="submit" value="Register" />
-                </div>
-            </form>
 
+            <div>
+                <div>
+                    <h2 className='text-center text-3xl'>Register Champ!! </h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 animate__animated animate__fadeInBottomRight">
+                            {/* name */}
+                            <div className="form-control ">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" {...register("name", { required: true })}
+                                    name="name" placeholder="Name"
+                                    className="input border-4 input-bordered" />
+                                {errors.name && <span className="text-red-600">Name is required</span>}
+                            </div>
+                            {/* email */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input border-4 input-bordered" />
+                                {errors.email && <span className="text-red-600">Email is required</span>}
+
+                            </div>
+                            {/* password */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password"  {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    pattern: /^(?=.*[A-Z])(?=.*[\W_]).{6,}$/
+                                })}
+                                    name="password"
+                                    placeholder="password" className="input input-bordered" />
+
+                                {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase  and a special character.</p>}
+                            </div>
+                            {/* confirm password */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Confirm Password</span>
+                                </label>
+                                <input type="password" {...register('confirmPassword',
+                                    {
+                                        required: true,
+                                        validate: (value) => value === password.current || "Passwords do not match"
+
+                                    }
+                                )} name="confirmPassword"
+                                    placeholder="password"
+                                    className="input input-bordered" />
+                                {errors.confirmPassword?.type === 'required' && <p className="text-red-600">Confirm Password is required</p>}
+                                <p className="text-red-600">{errors.confirmPassword?.message}</p>
+                            </div>
+                            {/* Photo Url */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="text"   {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                                {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                            </div>
+                            {/* Gender */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Gender</span>
+                                </label>
+                                <input type="text" {...register("gender", { required: false })} name="gender" placeholder="Male/Female/Trans" className="input input-bordered" />
+                            </div>
+                            {/* Phone number */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Phone</span>
+                                </label>
+                                <input type="text" {...register("phone", { required: false })} name="phone" placeholder="phone" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Address</span>
+                                </label>
+                                <input type="text" {...register("address", { required: false })}
+                                    name="address" placeholder="address" className="input input-bordered" />
+                            </div>
+                        </div>
+                        <div className="form-control mt-6 mb-10 w-3/5  mx-auto">
+                            <input className="btn btn-warning btn-block" type="submit" value="Register" />
+                            <div className="mt-2 mx-auto">
+                                Already a champ? <Link to="/login" className="text-blue-600">Login</Link>
+                            </div>
+                        </div>
+                        <div>
+                            <SocialLogin></SocialLogin>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
     );
 };
 
