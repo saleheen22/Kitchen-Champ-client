5a6385af139c2db5e4f05d0b4de743a8b4184178
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-06-12 15:31:11 +0600

    show all the approved classes on the class page and get button diasbled function incorporated

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,24 +1,115 @@
+import { HashLoader } from "react-spinners";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../hooks/useAuth";
+import { useState } from "react";
 
 const Classes = () => {
+    const [data1, setData1] = useState();
+    const [check, setCheck] = useState(true);
+    const [axiosSecure] = useAxiosSecure();
+    const [filteredUser, setFilteredUser] = useState('');
+
+    const { user } = useAuth();
+    const userEmail = user?.email;
+
+
+
+
+
+
+
+
+
+
+
+
+    const { data: cls = [], isLoading } = useQuery(
+        ['cls'], async () => {
+            const res = await axiosSecure.get("/class/approve")
+            return res.data;
+        })
+    console.log(cls);
+
+
+    if (check && userEmail) {
+        fetch('http://localhost:5000/allusers')
+            .then(res => res.json())
+            .then(data => {
+                setCheck(false)
+                console.log('this is the data', data);
+                data.map(d => {
+                    console.log('this is d.email', d.email)
+                    console.log('this is userEmail', userEmail)
+                    if (d.email == userEmail) {
+                        console.log('MILSEEEEEEEEEEE')
+                        setFilteredUser(d.role)
+                    }
+                })
+
+            })
+    }
+
+
+
+    if (isLoading) {
+        return <>
+            <HashLoader className="text-center text-5xl mx-auto my-12" color="rgba(214, 189, 54, 0.86)"></HashLoader>
+        </>
+    }
+
+
     return (
-        <div className="mt-28 max-w-screen-xl mx-auto">
+        <div className="mt-40 max-w-screen-xl mx-auto">
             <h2 className="text-3xl text-center mt-14 ">All Our Classes</h2>
-            <div className="max-h-screen">
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">
-                            Shoes!
-                            <div className="badge badge-secondary">NEW</div>
-                        </h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                            <div className="badge badge-outline">Fashion</div>
-                            <div className="badge badge-outline">Products</div>
+
+            <div className="grid grid-cols-3 gap-7">
+                {
+                    cls.map(c => <>
+                        <div key={c._id} className="max-h-screen">
+                            <div className="card w-96 h-full bg-base-100 shadow-xl">
+                                <figure><img className="card-image" src={c.image} alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">
+                                        {c.className}
+                                        {/* <div className="badge badge-secondary">Popular</div> */}
+                                    </h2>
+                                    <div className="">
+                                        <p><span>Instructor :</span>{c.instructorName}</p>
+                                        <p><span>Seats: </span>{c.seats}</p>
+                                        <p> <span>Price : $</span>{c.price}</p>
+                                        <p></p>
+                                    </div>
+                                    <div className="card-actions justify-end">
+                                        {
+                                            user ? <>
+                                            
+                                                {filteredUser === 'Student' ? <>
+                                                    <button className="btn btn-warning" disabled={false}>Add</button>
+                                                </> : <>
+                                                    <button className="btn btn-warning" disabled={true}>Add</button>
+                                                </>}
+                                            
+                                            </>:
+                                            <>
+                                            <button className="btn btn-warning">Add</button>
+                                            </>
+                                }
+                                        {/* <div className="badge badge-outline">Fashion</div>
+                                 <div className="badge badge-outline">Products</div> */}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </>)
+                }
             </div>
+
+
+            <div>
+
+            </div>
+
         </div>
     );
 };
--- src/pages/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors.jsx
@@ -2,7 +2,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { HashLoader } from "react-spinners";
-
+import './Instructor.css'
 const Instructors = () => {
     const [axiosSecure] = useAxiosSecure();
     const { data: instructors = [], isLoading } = useQuery(
@@ -10,32 +10,34 @@ const Instructors = () => {
             const res = await axiosSecure.get("/instructors1")
             return res.data;
         })
+        console.log(instructors);
 
-        console.log(instructors)
-    if(isLoading){
+    console.log(instructors)
+    if (isLoading) {
         return <>
-        <HashLoader className="text-center text-5xl mx-auto my-12" color="rgba(214, 189, 54, 0.86)"></HashLoader>
+            <HashLoader className="text-center text-5xl mx-auto my-12" color="rgba(214, 189, 54, 0.86)"></HashLoader>
         </>
     }
     return (
-      <div className="mt-28 max-w-screen-xl mx-auto">
-          <div className="grid grid-cols-3 gap-5">
-           {
-            instructors.map(inst => <>
-             <div className="card card-side bg-base-100 shadow-xl">
-                <figure><img src={inst.pic} alt="Movie" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">New movie is released!</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Watch</button>
-                    </div>
-                </div>
+        <div className="mt-36 max-w-screen-xl mx-auto">
+            <div className="grid grid-cols-3 gap-5">
+                {
+                    instructors.map(inst => <>
+                        <div className="card card-compact w-96 bg-base-100 shadow-xl h-auto">
+                            <figure><img className="card-image" src={inst.pic} alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{inst.name}</h2>
+                                <p>Email : <span> {inst.email}</span></p>
+                                
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-warning">Connect</button>
+                                </div>
+                            </div>
+                        </div>
+                    </>)
+                }
             </div>
-            </>)
-           }
         </div>
-      </div>
     );
 };
 
