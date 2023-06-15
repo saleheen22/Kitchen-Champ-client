b5123f0cb09b2d80a187b9a043b99953d6f6dc5d
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2023-06-15 20:07:05 +0600

    the button is fixed in the banner slider and linked to all the classes route

--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import gif from '../../../../public/logo/chef1.png'
 import gif2 from '../../../../public/logo/multiple-chefs.png'
 import gif1 from '../../../../public/logo/chef.png'
+import { Link } from 'react-router-dom';
 
 const Slider = () => {
     var settings = {
@@ -22,7 +23,7 @@ const Slider = () => {
       <div className='grid md:grid-cols-2'>
       <div className='md:w-3/4 pt-14 md:pt-24 md:mx-auto text-justify  mx-14 '>
        <p>Attention students! Do not let your summer break slip away. Turn it into a productive culinary exploration with Kitchen Champ. Hone your cooking skills, expand culinary knowledge, and unleash creativity in the kitchen. Gain confidence as you whip up mouthwatering dishes, impressing family and friends. Whether a beginner or budding chef, Kitchen Champ offers a summer of culinary growth, inspiration, and delicious achievements. Transform your break into a memorable culinary adventure!</p>
-       <button className='btn btn-warning mt-5 mb-5'> button </button>
+       <Link to = "/classes"> <button className='btn btn-warning mt-4 mb-4'> Enroll Now </button></Link>
       </div>
       
           <div className='ms-10 order-first md:order-last'>
@@ -39,7 +40,7 @@ const Slider = () => {
       <div className='grid md:grid-cols-2'>
       <div className='md:w-3/4 pt-14 md:pt-24 md:mx-auto text-justify  mx-14 h-1/2'>
        <p>Embark on a culinary adventure guided by acclaimed chefs from five-star hotels. Our renowned instructors, including Michelin-starred chefs and culinary competition winners, bring their expertise and passion to your kitchen. Learn their specialty dishes, gain insights, and refine your skills to become a true Kitchen Champ. Immerse yourself in their culinary wisdom, unlocking your potential and igniting your creativity. Join us on this remarkable journey of cooking, learning, and inspiration. Get ready to elevate your culinary prowess with our esteemed chefs as your mentors!</p>
-       <button className='btn btn-warning mt-4 mb-4'> button </button>
+      <Link to = "/classes"> <button className='btn btn-warning mt-4 mb-4'> Enroll Now </button></Link>
       </div>
       
           <div className='ms-10 align-items-center order-first md:order-last'>
